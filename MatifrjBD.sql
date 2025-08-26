e43621668f0bed1c52a3de843fcf1a5785fceb82
YuriLaCaB <161161186+YuriLaCaB@users.noreply.github.com>
2025-08-26 17:19:43 -0300

    Nome da turma no usuario comum

--- MatifrjBD.sql
+++ MatifrjBD.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `alternativas`
 --
 
+CREATE DATABASE IF NOT EXISTS `matifrjBD` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `matifrjBD`;
+
 DROP TABLE IF EXISTS `alternativas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
