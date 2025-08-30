2b7c16169d70c4ab56b7c6c086c73380ab7c7f51
Yan <martinsportofigueirayan@gmail.com>
2025-08-30 18:09:34 -0300

    Avisos melhorados

--- MatifrjBD.sql
+++ MatifrjBD.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE IF NOT EXISTS `matifrjbd` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+
 -- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
 --
 -- Host: localhost    Database: matifrjbd
